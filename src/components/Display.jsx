6babfd08d0f519dba8321e58b9037eb5fcca0be1
Garima Thapa <garima.thapa30@gmail.com>
2024-09-01 10:12:24 +0545

    calculator

---
+++ src/components/Display.jsx
@@ -0,0 +1,14 @@
+import styles from "./Display.module.css";
+
+function Display({ displayValue }) {
+  return (
+    <input
+      className={styles.display}
+      type="text"
+      value={displayValue}
+      readOnly
+    ></input>
+  );
+}
+
+export default Display;
